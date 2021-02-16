eddc305bb1515cf4f884320aabf641d1754eae86
Henrique Moraes <henriquemsm@outlook.com>
2021-02-16 18:24:13 -0300

    Estudando React - Componente Menu

--- pages/index.js
+++ pages/index.js
@@ -7,35 +7,80 @@ const Title = styled.h1`
 `;
 
 export default function Home() {
+  // itens é um array de objetos que contém o nome do item e o caminho
+
   return (
     <>
-      <header>
-        <div className="Popup"></div>
-        <Menu>
-          <Menu.ul>
-            <Menu.li>WOMEN</Menu.li>
-            <Menu.li>MEN</Menu.li>
-            <Menu.li>OBJECTS</Menu.li>
-            <Menu.li>JOURNAL</Menu.li>
-            <Menu.li>OUR STORY</Menu.li>
-            <Menu.li>🛒(0)</Menu.li>
-          </Menu.ul>
-        </Menu>
-        <img
-          src="https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
-          alt="shoes"
-          style={{
-            display: "block",
-            width: "1080px",
-            height: "720px",
-            margin: "0 auto",
-          }}
-        ></img>
-      </header>
-      <main>
-        <section></section>
-      </main>
-      <footer></footer>
+      <Menu
+        itens={[
+          { nome: "Product", path: "#" },
+          { nome: "Pricing", path: "#" },
+          { nome: "Sucess Stories", path: "#" },
+          { nome: "Services", path: "#" },
+          { nome: "About Us", path: "#" },
+          { nome: "Resources", path: "#" },
+        ]}
+        disp="inline-block" // Define disposição do menu
+      />
+      <button
+        style={{
+          padding: "12px 16px",
+          border: "solid black 2px",
+          borderRadius: ".5rem",
+          fontFamily: "Arial",
+          fontWeight: "bolder",
+        }}
+      >
+        Log In
+      </button>
+      <button
+        style={{
+          color: "white",
+          backgroundColor: "black",
+          marginLeft: "20px",
+          padding: "12px 30px",
+          border: "solid black 2px",
+          borderRadius: ".5rem",
+          fontFamily: "Arial",
+          fontWeight: "bolder",
+        }}
+      >
+        Create a free account
+      </button>
     </>
   );
 }
+
+// export default function Home() {
+//   return (
+//     <>
+//       <header>
+//         <div className="Popup"></div>
+//         <Menu>
+//           <Menu.ul>
+//             <Menu.li>WOMEN</Menu.li>
+//             <Menu.li>MEN</Menu.li>
+//             <Menu.li>OBJECTS</Menu.li>
+//             <Menu.li>JOURNAL</Menu.li>
+//             <Menu.li>OUR STORY</Menu.li>
+//             <Menu.li>🛒(0)</Menu.li>
+//           </Menu.ul>
+//         </Menu>
+//         <img
+//           src="https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
+//           alt="shoes"
+//           style={{
+//             display: "block",
+//             width: "1080px",
+//             height: "720px",
+//             margin: "0 auto",
+//           }}
+//         ></img>
+//       </header>
+//       <main>
+//         <section></section>
+//       </main>
+//       <footer></footer>
+//     </>
+//   );
+// }
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,19 +1,48 @@
 import React from "react";
 import styled from "styled-components";
 
-const Menu = styled.nav`
-  // Propriedades
-  background-color: ${({ theme }) => theme.colors.bg};
-  color: white;
-`;
-
-Menu.ul = styled.ul`
-  padding-left: 10px;
-`;
-
-Menu.li = styled.li`
+const MenuNav = styled.nav`
+  font-family: Arial;
+  font-weight: bolder;
+  background-color: white;
+  color: black;
   display: inline-block;
-  margin: 0 10px;
+  margin: 15px 120px 15px 120px;
+
+  ul {
+  }
+
+  li {
+    display: inline-block;
+    margin-left: 25px;
+  }
+
+  a {
+    text-decoration-color: lightgray;
+    text-decoration-thickness: 1.8px;
+  }
+
+  a:hover {
+    text-decoration-color: cyan;
+    text-decoration-thickness: 2.8px;
+  }
+  a:visited {
+    color: black;
+  }
 `;
 
-export default Menu;
+export default function Menu({ itens, disp }) {
+  return (
+    <>
+      <MenuNav style={{ display: disp }}>
+        <ul>
+          {itens.map((item, i) => (
+            <li key={`lista--${i}`}>
+              <a href={item.path}>{item.nome}</a>
+            </li>
+          ))}
+        </ul>
+      </MenuNav>
+    </>
+  );
+}
