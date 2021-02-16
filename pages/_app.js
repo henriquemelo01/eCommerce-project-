f49f7f9b3e0fc875f1cfed960641c4d042b26d7c
Henrique Moraes <henriquemsm@outlook.com>
2021-02-16 18:46:04 -0300

    Adicionando elemento Head ao _app.js + Add font externa e fav icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import { createGlobalStyle, ThemeProvider } from "styled-components";
+import Head from "next/head";
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
@@ -18,6 +22,19 @@ const theme = {
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>e-Commerce</title>
+        <link
+          rel="icon"
+          type="imagem/png"
+          href="https://www.favicon.cc/logo3d/329774.png"
+        />
+        <link rel="preconnect" href="https://fonts. gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
